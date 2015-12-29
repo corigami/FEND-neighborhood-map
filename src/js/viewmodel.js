a2f625406af8cebcdfd5c23a1f719b8f708df6d1
Corey Willinger <corey.willinger@gmail.com>
2015-12-28 21:01:44 -0500

    Revert "Revert "feat: implemented pins and infoWindow on menu click""
    This reverts commit 40c2694147a462371aa616eafa9b1958aa010ca6.

--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -5,6 +5,8 @@ app.ViewModel = function () {
     var self = this;
     var currentLocation = '';
     var map;
+    var infoWindow = '';
+    var infoWindowContent = '';
 
     this.menuList = ko.observableArray();
 
@@ -38,11 +40,11 @@ app.ViewModel = function () {
 
         // Sets the boundaries of the map based on pin locations
         window.mapBounds = new google.maps.LatLngBounds();
-        placePins(getLocStrings());
+        placePins(self.menuList());
     };
 
     /*
-    pinPoster(locations) takes in the array of locations created by locationFinder()
+    getPins(locations) takes in the array of locations created by locationFinder()
     and fires off Google place searches for each location
     */
     getPins = function (locations) {
@@ -61,21 +63,8 @@ app.ViewModel = function () {
 
             // Actually searches the Google Maps API for location data and runs the callback
             // function with the search results after each search.
-            service.textSearch(request, callback);
+            service.textSearch(request, place.callback);
         });
-    };
-
-    /*
-    getLocStrings() returns an array of every location string from the JSONs
-    written for bio, education, and work.
-    */
-    getLocStrings = function () {
-        var locations = [];
-        app.model.getAllLocations().forEach(function (loc) {
-            var locStr = loc.address + ', ' + loc.city;
-            locations.push(locStr);
-        });
-        return locations;
     };
 
     /*
@@ -83,34 +72,22 @@ app.ViewModel = function () {
     and fires off Google place searches for each location
     */
     placePins = function (locations) {
-
         // creates a Google place search service object. PlacesService does the work of
         // actually searching for location data.
         var service = new google.maps.places.PlacesService(self.map);
 
         // Iterates through the array of locations, creates a search object for each location
         locations.forEach(function (place) {
-
             // the search request object
             var request = {
-                query: place
+                query: place.queryString()
             };
 
             // Actually searches the Google Maps API for location data and runs the callback
             // function with the search results after each search.
-            service.textSearch(request, callback);
+            service.textSearch(request, place.gCallback);
         });
     };
-
-    /*
-    callback(results, status) makes sure the search returned results for a location.
-    If so, it creates a new map marker for that location.
-    */
-    callback = function (results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            createMarker(results[0]);
-        }
-    }
 
     /*
     createMapMarker(placeData) reads Google Places search results to create map pins.
@@ -128,17 +105,19 @@ app.ViewModel = function () {
                 map: self.map,
                 position: placeData.geometry.location,
                 title: name
-            }),
-            // infoWindows are the little helper windows that open when you click
-            // or hover over a pin on a map. They usually contain more information
-            // about a location.
-            infoWindow = new google.maps.InfoWindow({
-                content: '<div id="markerName">' +
-                    '<b>' + name + '</b>' +
-                    '</div>'
             });
+        // infoWindows are the little helper windows that open when you click
+        // or hover over a pin on a map. They usually contain more information
+        // about a location.
+
+        self.infoWindow = new google.maps.InfoWindow({
+            content: '<div id="markerName">' +
+                '<b>' + name + '</b>' +
+                '</div>'
+        })
+
         google.maps.event.addListener(marker, 'click', function () {
-            infoWindow.open(self.map, marker);
+            self.infoWindow.open(self.map, marker);
         });
 
         // this is where the pin actually gets added to the map.
@@ -146,11 +125,52 @@ app.ViewModel = function () {
         bounds.extend(new google.maps.LatLng(lat, lon));
         self.map.fitBounds(bounds); // fit the map to the new marker
         self.map.setCenter(bounds.getCenter()); // center the map
+        return marker;
+    };
+
+    onClick = function () {
+        if (self.currentLocation) {
+            self.currentLocation.bounceOff();
+            self.infoWindow.close();
+        }
+        self.currentLocation = this;
+        this.bounceOn();
+        self.infoWindow.set
+        self.infoWindow.setContent(self.currentLocation.pinContent);
+        self.infoWindow.open(this.pin.map, this.pin);
     }
 
     window.addEventListener('load', initMap);
 
 };
+
+
+var MenuItem = function (data) {
+    var self = this;
+    this.name = ko.observable(data.name);
+    this.city = ko.observable(data.city);
+    this.address = ko.observable(data.address);
+    this.queryString = ko.computed(function () {
+        return this.address() + ', ' + this.city() + ', USA';
+    }, this);
+    this.visable = ko.observable(true);
+    this.pin = '';
+
+    this.gCallback = function (results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            self.pin = createMarker(results[0]);
+        }
+    };
+    this.bounceOn = function () {
+        self.pin.setAnimation(google.maps.Animation.BOUNCE);
+    }
+    this.bounceOff = function () {
+        self.pin.setAnimation(null);
+    }
+    this.pinContent = '<div id="markerName">' +
+        '<b>' + self.name() + '</b>' +
+        '</div>';
+}
 
 //bind the view to our ViewModel
 ko.applyBindings(new app.ViewModel());
